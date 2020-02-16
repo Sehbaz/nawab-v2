41f65ba8f8b1b9ad3dbfb433f109d3e876f19827
Sehbaz Rafik <sehbazm@gmail.com>
2020-02-16 12:26:41 +0000

    basic routind and initial files added

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/main";
+import Navbar from "./components/navbar";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Show";
+
+function App() {
+  return (
+    <HashRouter>
+      <div className='App'>
+        <h1>Simple SPA</h1>
+        <ul className='header'>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/stuff'>Stuff</NavLink>
+          </li>
+          <li>
+            <NavLink to='/contact'>Contact</NavLink>
+          </li>
+        </ul>
+        <div className='content'>
+          {" "}
+          <Route exact path='/' component={Home} />
+          <Route path='/stuff' component={Stuff} />
+        </div>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Navbar() {
+  return <h1>Hello from navbar</h1>;
+}
+export default Navbar;
