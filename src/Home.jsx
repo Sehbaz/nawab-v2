1b3057fb9049cd0f4445d92064c3600569f29e6d
Sehbaz Rafik <sehbazm@gmail.com>
2020-02-16 15:03:47 +0000

    main page image added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import MainImg from "./images/car.png";
 
 class Home extends Component {
   render() {
@@ -6,15 +7,27 @@ class Home extends Component {
       <div className='container'>
         <div className='main-container'>
           <div>
-            <h1 style={{ fontSize: `4rem`, color: `white` }}>
-              Let's Get You On The Road
-            </h1>
-            <h4>
-              {" "}
-              Our driving school is a pupil focused school. Driving instructors
-              are fully DVSA qualified and Approved Driving Instructors (ADI’s)
-              and provide a professional and courteous service.
-            </h4>
+            <div>
+              <h1 style={{ fontSize: `3rem`, color: `white` }}>
+                Let's Get You On The Road
+              </h1>
+              <h4>
+                {" "}
+                Our driving school is a pupil focused school. Driving
+                instructors are fully DVSA qualified and Approved Driving
+                Instructors (ADI’s) and provide a professional and courteous
+                service.
+              </h4>
+            </div>
+            <div className='main-img-container'>
+              <div style={{ display: `flex`, justifyContent: `center` }}>
+                <img
+                  src={MainImg}
+                  alt='main-image'
+                  style={{ width: `35%` }}
+                ></img>
+              </div>
+            </div>
           </div>
         </div>
         <div
