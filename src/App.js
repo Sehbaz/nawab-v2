522f48801934f777cd3308fe8cfcb63a2a76a1da
Sehbaz Rafik <sehbazm@gmail.com>
2020-02-26 07:44:51 +0000

    git logo update

--- src/App.js
+++ src/App.js
@@ -16,20 +16,24 @@ function App() {
           <div>
             <h3>
               <NavLink to='/' style={{ color: `#00ce89` }}>
-                <img src={MainLogo} style={{ height: `18px` }}></img>
+                <img src={MainLogo} style={{ height: `1rem` }}></img>
               </NavLink>
             </h3>
           </div>
           <li>
-            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/' className='navlink'>
+              Home
+            </NavLink>
           </li>
           <li>
-            <a href={ShowDoc} target='_blank'>
+            <a href={ShowDoc} target='_blank' className='navlink'>
               Show/Tell
             </a>
           </li>
           <li>
-            <NavLink to='/route'>Route</NavLink>
+            <NavLink to='/route' className='navlink'>
+              Route
+            </NavLink>
           </li>
         </ul>
         <div className='content'>
