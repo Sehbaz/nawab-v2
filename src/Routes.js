1f14763e1998d30d416a1b88669096359705f3ce
Sehbaz Rafik <sehbazm@gmail.com>
2020-03-10 08:16:10 +0000

    route update

--- src/Routes.js
+++ src/Routes.js
@@ -45,71 +45,136 @@ class Routes extends Component {
                   <td>Pull up before last mini roundabout</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>
+                    Independent drive:- traffic light turn left Follow sign for
+                    M1/M69
+                  </td>
+                </tr>
+                <tr>
+                  <td>At Thurmaston roundabout Take 4th exit Barkbythope</td>
                 </tr>
                 <tr>
                   <td>Pull up before last mini roundabout</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>Mini roundabout straight</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>EOR right toward country lane Barkby road</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>End of rd right next left</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>Carry on country lane Hamilton lane</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>Turn right Keyham lane west</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>Roundabout (Maidenwell ave) 1st exit</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>Roundabout (Hamilton way)2nd exit</td>
                 </tr>
                 <tr>
-                  <td>Pull up before last mini roundabout</td>
+                  <td>Turn right back to test centre.</td>
                 </tr>
               </table>
             </div>
             <div className='route-box'>
               <table style={{ width: `100%` }}>
                 <tr>
-                  <th style={{ textAlign: "center" }}>( 1 )</th>
+                  <th style={{ textAlign: "center" }}>( 2 )</th>
                 </tr>
                 <tr>
-                  <td>Test centre turn right (Cannock st)</td>
+                  <td>
+                    Out of the carpark and turn right into Cannock Street turn
+                    left into Wenlock Way
+                  </td>
                 </tr>
                 <tr>
-                  <td>EOR turn left (Barkby Thorpe Rd)</td>
+                  <td>End of the road turn left into Thurmaston Road</td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>
+                    Turn right on the roundabout 4th exit into Victoria Road
+                    East
+                  </td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>Follow the road into Tailby Road</td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>Turn left into Wycombe Road</td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>Mini roundabout turn right into Humberstone Drive</td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>End of the road Turn left into Uppingham Road</td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>
+                    Follow the road ahead from the right hand lane towards
+                  </td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>Peterborough A47 into Uppingham Road</td>
                 </tr>
                 <tr>
-                  <td>Sehbaz</td>
+                  <td>Turn left into lodge farm Road</td>
+                </tr>
+                <tr>
+                  <td>Pull up in the left (Downhill Start)</td>
+                </tr>
+                <tr>
+                  <td>Pull up on the left (Angle Start)</td>
+                </tr>
+                <tr>
+                  <td>Turn left into Dakyn Road</td>
+                </tr>
+                <tr>
+                  <td>Pull up on the left (Parallel Parking Exercise)</td>
+                </tr>
+                <tr>
+                  <td>Turn left into Dudley Avenue</td>
+                </tr>
+                <tr>
+                  <td>End of the road turn right into Ocean Road</td>
+                </tr>
+                <tr>
+                  <td>Pull up on the left</td>
+                </tr>
+                <tr>
+                  <td>Start of the Independent Driving</td>
+                </tr>
+                <tr>
+                  <td>End of the road turn left (Thurncourt Road)</td>
+                </tr>
+                <tr>
+                  <td>End of the road turn left again (Scraptoft Lane)</td>
+                </tr>
+                <tr>
+                  <td>Follow the signs for Ring Road North</td>
+                </tr>
+                <tr>
+                  <td>Follow the signs for Ring Road</td>
+                </tr>
+                <tr>
+                  <td>End of independent Driving on Thurmaston Lane</td>
+                </tr>
+                <tr>
+                  <td>Turn right on the roundabout into Humberstone Lane</td>
+                </tr>
+                <tr>
+                  <td>Turn right into Barkbythorpe Road</td>
+                </tr>
+                <tr>
+                  <td>Turn right into Cannock Street</td>
+                </tr>
+                <tr>
+                  <td>Turn left into test centre carpark</td>
                 </tr>
               </table>
             </div>
